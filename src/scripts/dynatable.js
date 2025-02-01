8ed67f36b4df570084ed14b2a21b02828a8d1ee2
blackkat98 <nam.th.200698@gmail.com>
2025-02-02 00:45:43 +0700

    Add buildHtmlHeader function

--- src/scripts/dynatable.js
+++ src/scripts/dynatable.js
@@ -1,10 +1,11 @@
 import $ from 'jquery'
 import { uid } from 'uid'
+import _ from 'lodash'
 
 export class DynaTable
 {
     constructor(options) {
-        this.tableId = `dyna-table-${uid(10)}`
+        this.tableId = uid(10)
         this.headerDepth = 1
         this.headerArray = []
 
@@ -14,23 +15,41 @@ export class DynaTable
     }
 
     draw() {
-        const container = $(this.containerId.startsWith('#') ? this.containerId : `#${this.containerId}`)
-        container.html(`
-            <table id="${this.tableId}" style="width: 100%;">
-                <thead></thead>
-                <tbody></tbody>
-                <tfoot></tfoot>
+        const htmlHeader = this.buildHtmlHeader()
+        const html = `
+            <table id="table-${this.tableId}" style="width: 100%;">
+                <thead id="thead-${this.tableId}">${htmlHeader}</thead>
+                <tbody id="tbody-${this.tableId}"></tbody>
+                <tfoot id="tfoot-${this.tableId}"></tfoot>
             </table>
-        `)
+        `
+        const container = $(this.containerId.startsWith('#') ? this.containerId : `#${this.containerId}`)
+        container.html(html)
         const table = $(`#${this.tableId}`)
-        this.expandColumnHeaderSettings(this.columns)
-        console.log(this.columns)
-        console.log(this.headerDepth)
     }
 
-    drawHeader() {
+    buildHtmlHeader() {
         this.expandColumnHeaderSettings(this.columns)
+        this.headerArray = Array.from(Array(this.headerDepth)).map(el => [])
+        this.buildColumnHeaderArray(this.columns)
+        const htmlRows = this.headerArray.map(row => {
+            const htmlCells = row.map(cell => {
+                const closingTag = '</th>'
+                let openingTag = '<th'
 
+                if (cell.colSpan > 1) openingTag += ` colspan="${cell.colSpan}"`
+                if (cell.rowSpan > 1) openingTag += ` rowspan="${cell.rowSpan}"`
+
+                openingTag += '>'
+
+                return cell.label ? openingTag + cell.label + closingTag : openingTag + closingTag
+            }).join('')
+
+            return `<tr>${htmlCells}</tr>`
+        })
+        const htmlHeader = htmlRows.join('')
+
+        return htmlHeader
     }
 
     expandColumnHeaderSettings(columns) {
@@ -51,13 +70,29 @@ export class DynaTable
                             ...columns[i].children[j],
                             label: el,
                         }))
-                        columns[i].children.splice(j, 1, newChildren)
+                        columns[i].children.splice(j, 1, ...newChildren)
                         columns[i].colSpan += newChildren.length - 1
                     }
                 }
 
                 this.expandColumnHeaderSettings(columns[i].children)
             }
+        }
+    }
+
+    buildColumnHeaderArray(columns) {
+        for (let i = 0; i < columns.length; i++) {
+            const colSpan = columns[i].colSpan
+            const rowIndex = columns[i].depth - 1
+            const rowSpan = columns[i].children && columns[i].children.length ? 1 : (this.headerDepth - columns[i].depth + 1)
+
+            for (let j = rowIndex; j < rowIndex + rowSpan; j++) {
+                this.headerArray[j].push(j === rowIndex ? _.cloneDeep({ ...columns[i], rowSpan: rowSpan, children: [] }) : { colSpan: 1, rowSpan: 1 })
+            }
+
+            if (colSpan > 1) this.headerArray[rowIndex] = this.headerArray[rowIndex].concat(Array.from(Array(colSpan - 1)).map(el => ({ colSpan: 1, rowSpan: 1 })))
+
+            if (columns[i].children && columns[i].children.length) this.buildColumnHeaderArray(columns[i].children)
         }
     }
 }
